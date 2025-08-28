28a74a06bafc3ea8cc90cd1487602659bee48084
istiyak-ahmmod <istiyak3250@gmail.com>
2025-08-28 20:40:49 +0600

    heart count function add

---
+++ javacode.js
@@ -0,0 +1,102 @@
+// function to get innerText value 
+
+function getInnerText(id) {
+    let innerText = parseInt(document.getElementById(id).innerText);
+    return innerText;
+}
+
+// function to Set innerText value
+
+
+function setInnerText(value) {
+    let countElemet = document.getElementById("heartCount")
+    countElemet.innerText = value
+}
+// National Emergency Number 
+
+document.getElementById("nationalClick").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+    
+    
+})
+//   <!-- Police --> 
+document.getElementById("police-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+    
+})
+//   <!-- fire-service --> 
+document.getElementById("fire-service-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+ 
+})
+//   <!-- ambulance --> 
+document.getElementById("ambulance-click").addEventListener("click",function (){
+    const heat =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+ 
+})
+//   <!-- Women & Child --> 
+document.getElementById("Women-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+ 
+})
+//  <!-- Anti Corruption --> 
+document.getElementById("anti-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+})
+//  <!-- Electricity --> 
+document.getElementById("electricity-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+})
+//  <!-- Brac --> 
+document.getElementById("brac-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+})
+//  <!-- Railway --> 
+document.getElementById("railway-click").addEventListener("click",function (){
+    const heart =getInnerText("heartCount");
+   
+    const heartCount = heart + 1;
+
+    setInnerText(heartCount);
+    
+})
